b8d3c9d128182fa7fd260e199afbaf468e3867ff
Sapan <SKumarDas@luxoft.com>
2018-03-13 17:07:18 +0100

    Add main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/*************************************
+ * file:     main.c
+ * Engineer: Sapan Das
+ * Date:     13th March 2018
+ * Company:  Pelagicore AB
+ *************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello this is my 1st jenkin job that is gonna be executing\n");
+	return 0;
+}
+
+
